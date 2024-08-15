aea0e40606b4be8382dad9e6ded9bc22d69adbb7
shabarie <shabariework@gmail.com>
2024-08-16 02:01:25 +0530

    navbar component formated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,24 +6,24 @@ const navbar = () => {
 
   const [sticky, setSticky] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
 
-    window.addEventListener('scroll',()=>{
+    window.addEventListener('scroll', () => {
       window.scrollY > 50 ? setSticky(true) : setSticky(false);
     })
-  },[]);
+  }, []);
 
   return (
-   <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-        <img src={logo} alt="" className='logo' />
-        <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About us</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
-            <li><button className='btn'>Contact us</button></li>
-        </ul>
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+      <img src={logo} alt="" className='logo' />
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li><button className='btn'>Contact us</button></li>
+      </ul>
     </nav>
   )
 }
